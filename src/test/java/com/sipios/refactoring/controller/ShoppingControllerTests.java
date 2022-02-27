aeda5f19b81821c5272b677065058b65638633be
GUETIN Arnaud <guetin.arnaud@gmail.com>
2022-02-28 00:58:26 +0100

    develop-refacto / modify unit test

--- src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
+++ src/test/java/com/sipios/refactoring/controller/ShoppingControllerTests.java
@@ -3,19 +3,24 @@ package com.sipios.refactoring.controller;
 import com.sipios.refactoring.UnitTest;
 import com.sipios.refactoring.controller.entities.Body;
 import com.sipios.refactoring.controller.entities.Item;
+import com.sipios.refactoring.service.ShoppingService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 class ShoppingControllerTests extends UnitTest {
 
-    @InjectMocks
-    private ShoppingController controller;
+    @Autowired
+    private ShoppingService shoppingService;
 
     @Test
     void should_not_throw() {
         Assertions.assertDoesNotThrow(
-            () -> controller.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
+            () -> shoppingService.getPrice(new Body(new Item[] {}, "STANDARD_CUSTOMER"))
         );
     }
 }
